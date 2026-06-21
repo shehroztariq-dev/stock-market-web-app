171ef97867d3ef75f10f53d7dcbc8f74cdb79598
Shehroz Tariq <shehroztariq.dev@gmail.com>
2026-06-21 23:55:38 +0500

    feat: implement stock search

--- app/(root)/_components/nav-items.tsx
+++ app/(root)/_components/nav-items.tsx
@@ -1,22 +1,50 @@
 "use client";
 
-import Link from "next/link";
 import { NAV_ITEMS } from "@/lib/constants";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchCommand from "@/components/SearchCommand";
 
-export default function NavItems() {
+const NavItems = ({
+  initialStocks,
+}: {
+  initialStocks: StockWithWatchlistStatus[];
+}) => {
   const pathname = usePathname();
 
-  const isActice = (path: string) => {};
+  const isActive = (path: string) => {
+    if (path === "/") return pathname === "/";
+
+    return pathname.startsWith(path);
+  };
+
   return (
-    <ul className="flex gap-4">
-      {NAV_ITEMS.map((item) => (
-        <li key={item.href}>
-          <Link href={item.href} className="hover:text-cyan-500 text-gray-400">
-            {item.label}
-          </Link>
-        </li>
-      ))}
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+      {NAV_ITEMS.map(({ href, label }) => {
+        if (href === "/search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={initialStocks}
+              />
+            </li>
+          );
+
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={`hover:text-yellow-500 transition-colors ${
+                isActive(href) ? "text-gray-100" : ""
+              }`}>
+              {label}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
-}
+};
+export default NavItems;
